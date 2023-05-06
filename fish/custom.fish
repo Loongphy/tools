ad1b417b7b2ab419dc0e45ea5fb52564f3d3039e
Loongphy <Loongphy@outlook.com>
2023-05-07 00:04:17 +0800

    chore: git commands

--- fish/custom.fish
+++ fish/custom.fish
@@ -2,7 +2,12 @@
 alias -s py="python3"
 alias -s gnoe="git commit --amend --reset-author --no-edit"
 alias -s dcu="docker compose pull && docker compose up -d --remove-orphans"
-alias -s gf="git push -f"
+
+
+# git commands
+alias gp='git push'
+alias gpf='git push --force'
+alias gpl='git pull --rebase'
 alias -s gd="git branch -a | grep -v \"main\" | xargs git branch -D"
 
 # 杀死运行在指定端口的程序
