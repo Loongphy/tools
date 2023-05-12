0c798e796f086520fa04605dc8ea7d32c642d96b
Loongphy <Loongphy@outlook.com>
2023-05-13 01:19:16 +0800

    feat: proxy&nr dev

--- fish/custom.fish
+++ fish/custom.fish
@@ -1,17 +1,28 @@
 #!/usr/bin/env fish
+
 alias -s py="python3"
 alias -s gnoe="git commit --amend --reset-author --no-edit"
 alias -s dcu="docker compose pull && docker compose up -d --remove-orphans"
-
 
 # git commands
 alias gp='git push'
 alias gpf='git push --force'
 alias gpl='git pull --rebase'
-alias -s gd="git branch -a | grep -v \"main\" | xargs git branch -D"
+alias gd="git branch -a | grep -v \"main\" | xargs git branch -D"
 
-# 杀死运行在指定端口的程序
+# proxy
+function proxy
+    set ip (grep nameserver /etc/resolv.conf | cut -d ' ' -f 2)
+    export all_proxy=http://$ip:7890
+    git config --global http.proxy http://$ip:7890
+    git config --global https.proxy http://$ip:7890
+end
+
+# kill the process of the specified port
 function kp
     set port $argv[1]
     lsof -i:$port | awk 'NR==2{print $2}' | xargs kill -9
 end
+
+# frontend
+alias d="nr dev"
