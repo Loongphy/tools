ba5d9b5c5dd18ed06c6f4aac4f0bad5de8baa0af
Loongphy <Loongphy@outlook.com>
2023-05-14 18:42:23 +0800

    feat(fish): get ip address

--- config.fish
+++ config.fish
@@ -6,15 +6,16 @@ set PATH $HOME/.cargo/bin $PATH
 alias py="python3"
 
 # git commands
-alias gp='git push'
+alias gp="git push"
 alias gd="git branch -a | grep -v \"main\" | xargs git branch -D"
-alias gpf='git push --force'
-alias gpl='git pull --rebase'
+alias gpf="git push --force"
+alias gpl="git pull --rebase"
 alias gnoe="git commit --amend --reset-author --no-edit"
+alias ip="curl ipinfo.io"
 
 # proxy
 function proxy
-    set ip (grep nameserver /etc/resolv.conf | cut -d ' ' -f 2)
+    set ip (grep nameserver /etc/resolv.conf | cut -d " " -f 2)
     export all_proxy=http://$ip:7890
     git config --global http.proxy http://$ip:7890
     git config --global https.proxy http://$ip:7890
@@ -23,7 +24,7 @@ end
 # kill the process of the specified port
 function kp
     set port $argv[1]
-    lsof -i:$port | awk 'NR==2{print $2}' | xargs kill -9
+    lsof -i:$port | awk "NR==2{print $2}" | xargs kill -9
 end
 
 # frontend
